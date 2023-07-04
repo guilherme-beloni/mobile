b3a755afd4e192a3db5575043ea0865276f45957
Guilherme Zacarias Beloni <97005507+guilherme-beloni@users.noreply.github.com>
2023-07-03 21:04:45 -0300

    Inicial New Route

--- src/screens/New.tsx
+++ src/screens/New.tsx
@@ -1,4 +1,4 @@
-import { ScrollView, View, Text } from "react-native";
+import { ScrollView, View, Text, TextInput } from "react-native";
 import { BackButton } from "../components/BackButton";
 
 export function New () {
@@ -6,6 +6,18 @@ export function New () {
         <View className="flex-1 bg-background px-8 pt-16">
             <ScrollView showsHorizontalScrollIndicator={false}>
                 <BackButton />
+
+                <Text className="mt-6 text-white font-extrabold text-3xl">
+                    Criar Hábito
+                </Text>
+
+                <Text className="mt-6 text-white font-semibold text-base">
+                    Qual é o seu comprometimento?
+                </Text>
+
+                <TextInput className="h-12 pl-4 rounded-lg mt-3 bg bg-neutral-800 text-white focus:border-2 focus:border-violet-700">
+
+                </TextInput>
             </ScrollView>
         </View>
     )
